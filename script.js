c9341046fe651ce047a384d16714b792e32d80da
empecot <empecot@yahoo.com>
2016-02-23 17:34:10 -0500

    Fixed it!

--- script.js
+++ script.js
@@ -5,54 +5,25 @@ $('#enter_city').on('submit', function(e){
 
 	  $('body').removeClass();
 
-
-	   if (city === "nyc" || city === 'new york city' || city === 'new york') {
-        $('body').addClass('.nyc');
-    }
 // if input value equals the city name, change the background image to the designated img url for that city.
 
-
-    // if (city === "austin" || city === 'atx') {
-    //     $('body').addClass('austin');
-    // }
+	   if (city === "nyc" || city === 'new york city' || city === 'new york') {
+        $('body').addClass('nyc');
+    }
+   	   if (city === "austin" || city === 'atx') {
+        $('body').addClass('austin');
+    }
+	   if (city === "san francisco" || city === 'sf' || city === 'bay area') {
+        $('body').addClass('sf');
+    }
+	   else if (city === "sydney" || city === 'syd') {
+        $('body').addClass('sydney');
+	}
+	   if (city === "los angeles" || city === 'la' || city === 'lax') {
+        $('body').addClass('la');
+    }
 });
 
-
-
-
-
-
- // if (city === "nyc" || city === 'new york city' || city === 'new york') {
- //        $('body').addClass('.nyc');
- //    }
-
-// if (city === "Austin"){
-//  src = '../images/austin.jpg'}
-//  $('body').css('backgroundImage','url('+src+')');
-
-// });
-
-
-
-
-    
-
-
-// else if (user submits "Los Angeles" or "LA" or "LAX"){
-//  $('body').css('backgroundImage',../images/la.jpg);
-// }
-
-// else if (user submits "New York" or "New York City" or "NYC"){
-//  $('body').css('backgroundImage',../images/nyc.jpg);
-// }
-
-// else if (user submits "San Francisco" or "SF" or "Bay Area"){
-//  $('body').css('backgroundImage',../images/sf.jpg);
-// }
-
-// else if (user submits "Sydney" or "SYD"){
-//  $('body').css('backgroundImage',../images/sydney.jpg);
-// }
 
 
 
