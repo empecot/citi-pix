1f8e7012ed989b16eba5c318167fd727fec1f9aa
empecot <empecot@yahoo.com>
2016-02-23 15:49:23 -0500

    more attempts

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 // define function (on.submit) and define variable (input value). Retrieve input value. 
-$('#city-name').on('submit', function(e){
-	var city = $('#city-type').val();
+$('#enter_city').on('submit', function(e){
 	  e.preventDefault();
+	  var city = $('#city-type').val();
+
+	  $('body').removeClass();
+
+
+	   if (city === "nyc" || city === 'new york city' || city === 'new york') {
+        $('body').addClass('.nyc');
+    }
 // if input value equals the city name, change the background image to the designated img url for that city.
-if (city === "Austin"){
- src = '../images/austin.jpg'
- $('body').css('backgroundImage','url('+src+')');
-}
+
+
+    // if (city === "austin" || city === 'atx') {
+    //     $('body').addClass('austin');
+    // }
 });
 
+
+
+
+
+
+ // if (city === "nyc" || city === 'new york city' || city === 'new york') {
+ //        $('body').addClass('.nyc');
+ //    }
+
+// if (city === "Austin"){
+//  src = '../images/austin.jpg'}
+//  $('body').css('backgroundImage','url('+src+')');
+
+// });
+
+
+
+
+    
 
 
 // else if (user submits "Los Angeles" or "LA" or "LAX"){
