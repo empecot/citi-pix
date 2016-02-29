af32baa2f1bd124939cd38aeae84b74afa89e513
empecot <empecot@yahoo.com>
2016-02-29 14:35:12 -0500

    Attempted drop down menu

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// define function (on.submit) and define variable (input value). Retrieve input value. 
-$('#enter_city').on('submit', function(e){
+// Create an array with the following values: "NYC", "SF", "LA", "ATX", "SYD"; 
+// use the array to add values to the <select> menu by using a for loop in JavaScript (don't update the HTML to do this!)
+var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+    // console.log(cities[2]);
+for (var i = 0; i <cities.length; i++) {
+    // console.log(cities[i]);
+    $('.citi-options').append('<option val="'+cities[i]+'">cities[i]</option>');   
+};
+$('#enter_city').on('change', function(e){
 	  e.preventDefault();
 	  var city = $('#city-type').val();
 
@@ -10,22 +17,31 @@ $('#enter_city').on('submit', function(e){
 	   if (city === "nyc" || city === 'new york city' || city === 'new york') {
         $('body').addClass('nyc');
     }
-   	   if (city === "austin" || city === 'atx') {
+   	   else if (city === "austin" || city === 'atx') {
         $('body').addClass('austin');
     }
-	   if (city === "san francisco" || city === 'sf' || city === 'bay area') {
+	   else if (city === "san francisco" || city === 'sf' || city === 'bay area') {
         $('body').addClass('sf');
     }
 	   else if (city === "sydney" || city === 'syd') {
         $('body').addClass('sydney');
 	}
-	   if (city === "los angeles" || city === 'la' || city === 'lax') {
+	   else if (city === "los angeles" || city === 'la' || city === 'lax') {
         $('body').addClass('la');
     }
 });
 
 
 
+// When the user changes the input of the drop-down, update the background image based on what they selected
 
+// Use $.append() in your iteration on the drop-down menu
+
+// Use the $.attr() function to update html classes
+
+// Look up and use the $.change event handler to capture user actions
+// Get the value of user input using $.val()
+// Use  if/else conditionals to control the flow of your application
+// Display your pseudocode as Javascript comments
 
 
