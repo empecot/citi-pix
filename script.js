4aced9e28fb9980674041014307fe936f4947edc
empecot <empecot@yahoo.com>
2016-03-01 13:13:44 -0500

    Finished append code

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Create an array with the following values: "NYC", "SF", "LA", "ATX", "SYD"; 
-// use the array to add values to the <select> menu by using a for loop in JavaScript (don't update the HTML to do this!)
+
 var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
-    // console.log(cities[2]);
-for (var i = 0; i <cities.length; i++) {
-    // console.log(cities[i]);
-    $('.citi-options').append('<option val="'+cities[i]+'">cities[i]</option>');   
-};
+
+    $('#city-type').append('<option>Select a city!</option>');
+
+    for (var i = 0; i <cities.length; i++) {
+   $('#city-type').append('<option val=' +cities[i]+ '>'+ cities[i] + '</option>');
+}
+
 $('#enter_city').on('change', function(e){
 	  e.preventDefault();
-	  var city = $('#city-type').val();
+	  var city = $('#city-type').val().toLowerCase();
+      $('body').removeClass();
 
-	  $('body').removeClass();
-
-// if input value equals the city name, change the background image to the designated img url for that city.
 
 	   if (city === "nyc" || city === 'new york city' || city === 'new york') {
         $('body').addClass('nyc');
@@ -31,17 +30,5 @@ $('#enter_city').on('change', function(e){
     }
 });
 
-
-
-// When the user changes the input of the drop-down, update the background image based on what they selected
-
-// Use $.append() in your iteration on the drop-down menu
-
-// Use the $.attr() function to update html classes
-
-// Look up and use the $.change event handler to capture user actions
-// Get the value of user input using $.val()
-// Use  if/else conditionals to control the flow of your application
-// Display your pseudocode as Javascript comments
 
 
